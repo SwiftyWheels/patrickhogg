c146e2218bccf8841df478fe15c97aecefa8af66
Patrick <patrick.hogg@outlook.com>
2022-07-24 13:48:56 -0400

    Initial commit to my portfolio website.

---
+++ scripts/scripts.js
@@ -0,0 +1,4 @@
+// This places the JS in strict mode which will throw errors when the code
+// isn't written properly. Helps with debugging.
+// This can also be placed inside a function to make the function strict mode
+"use strict";
