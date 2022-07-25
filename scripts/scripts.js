b11ba5c9f87324c7baf223c4878e11cef63a5a51
Patrick <patrick.hogg@outlook.com>
2022-07-24 23:52:57 -0400

    Added tons of content to the site. Changed the hero container to display a repeating svg. Added a nav bar that works with mobile as well. Added an about me section with working toggle buttons to display different versions of the about me.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,3 +2,45 @@
 // isn't written properly. Helps with debugging.
 // This can also be placed inside a function to make the function strict mode
 "use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init(){
+    toggleIntros();
+}
+
+function toggleIntros(){
+
+    let aboutMeDiv = document.querySelector("#aboutMe");
+    let longIntroButton = aboutMeDiv.querySelector("#long-intro-button > p > span");
+    let tldrIntroButton = aboutMeDiv.querySelector("#tldr-intro-button > p > span");
+
+    let introDivs = [];
+    introDivs.push(longIntroButton.parentElement.parentElement.nextElementSibling);
+    introDivs.push(tldrIntroButton.parentElement.parentElement.nextElementSibling);
+
+    let handleClick = {
+        handleEvent(object) {
+            let target = object.target;
+            let belowDiv = target.parentElement.parentElement.nextElementSibling;
+
+            if (belowDiv.classList.contains("hidden")){
+                belowDiv.classList.remove("hidden");
+            }else{
+                belowDiv.classList.add("hidden");
+            }
+
+            for (const node of introDivs) {
+                if (node !== belowDiv) {
+                    if (!node.classList.contains("hidden")){
+                        node.classList.add("hidden");
+                    }
+                }
+            }
+
+        }
+    }
+
+    longIntroButton.addEventListener("click", handleClick);
+    tldrIntroButton.addEventListener("click", handleClick);
+}
